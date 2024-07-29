1975cf73b083d4234f48f5d13564bd8f9a8f14cd
nadana24-meet <nadanamouz@gmail.com>
2024-07-29 15:40:12 +0300

    edited index.html

--- templatemo_586_scholar/app.py
+++ templatemo_586_scholar/app.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template, redirect, request, session, url_for, j
 import pyrebase
 
 firebaseConfig = {
-  'apiKey': "AIzaSyDkOa607l4wYh9BcaAt8LdBuU8CL7wK6N0",
-  'authDomain': "case-studies---lissan.firebaseapp.com",
-  'databaseURL': "https://case-studies---lissan-default-rtdb.europe-west1.firebasedatabase.app",
-  'projectId': "case-studies---lissan",
-  'storageBucket': "case-studies---lissan.appspot.com",
-  'messagingSenderId': "904275628053",
-  'appId': "1:904275628053:web:f6b945dd1ead37fa6bbe1c",
-  'databaseURL': "https://case-studies---lissan-default-rtdb.europe-west1.firebasedatabase.app/"
+    'apiKey': "AIzaSyDkOa607l4wYh9BcaAt8LdBuU8CL7wK6N0",
+    'authDomain': "case-studies---lissan.firebaseapp.com",
+    'databaseURL': "https://case-studies---lissan-default-rtdb.europe-west1.firebasedatabase.app",
+    'projectId': "case-studies---lissan",
+    'storageBucket': "case-studies---lissan.appspot.com",
+    'messagingSenderId': "904275628053",
+    'appId': "1:904275628053:web:f6b945dd1ead37fa6bbe1c",
+    'databaseURL': "https://case-studies---lissan-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = '123456'
@@ -20,74 +19,82 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
 
-
-@app.route('/signup', methods=['GET','POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-  if request.method == 'GET' and session["user"] == None:
-    return render_template("signup.html")
-  elif request.method == 'POST':
-    try:
-      email = request.form['email']
-      password = request.form['password']
-      firstname = request.form['firstname']
-      lastname = request.form['lastname']
-      session['user'] = auth.create_user_with_email_and_password(email, password)
-      session['email'] = email
-      session['password'] = password
-      uid = session['user']['localId']
-      UserInfo = {'firstname':firstname, 'lastname':lastname}
-      db.child('users').child(uid).set(UserInfo)
-      return render_template("index.html")
-    except:
-      return render_template("error.html")
-  else:
-    return render_template("index.html")
+    if request.method == 'GET':
+        if not session.get("user"):
+            return render_template("signup.html")
+        else:
+            return redirect(url_for('index'))
 
+    elif request.method == 'POST':
+        try:
+            email = request.form['email']
+            password = request.form['password']
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            user = auth.create_user_with_email_and_password(email, password)
+            session['user'] = user
+            uid = user['localId']
+            UserInfo = {'firstname': firstname, 'lastname': lastname}
+            db.child('users').child(uid).set(UserInfo)
+            return redirect(url_for('index'))
+        except Exception as e:
+            print(e)
+            return render_template("error.html")
+    else:
+        return redirect(url_for('index'))
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'GET' and session["user"] == None:
-    return render_template("login.html")
-  elif request.method == 'POST':
-    try:
-      email = request.form['email']
-      password = request.form['password']
-      session['user'] = auth.sign_in_with_email_and_password(email, password)
-      return render_template("index.html")
-    except:
-      return render_template("error.html")
-  else:
-    return render_template ("index.html")
+    if request.method == 'GET':
+        if not session.get("user"):
+            return render_template("login.html")
+        else:
+            return redirect(url_for('index'))
 
+    elif request.method == 'POST':
+        try:
+            email = request.form['email']
+            password = request.form['password']
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = user
+            return redirect(url_for('index'))
+        except Exception as e:
+            print(e)
+            return render_template("error.html")
+    else:
+        return redirect(url_for('index'))
 
-@app.route('/index', methods=['GET','POST'])
+@app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
+# Uncomment and implement these routes if needed
 '''
-@app.route('/practice', methods=['GET','POST'])
+@app.route('/practice', methods=['GET', 'POST'])
 def practice():
-  return render_template('practice.html')
+    return render_template('practice.html')
 
-@app.route('/meetups', methods=['GET','POST'])
+@app.route('/meetups', methods=['GET', 'POST'])
 def meetups():
-  return render_template('meetups.html')
+    return render_template('meetups.html')
 
-@app.route('/jobs', methods=['GET','POST'])
+@app.route('/jobs', methods=['GET', 'POST'])
 def jobs():
-  return render_template('Jobs.html')
+    return render_template('jobs.html')
 
-@app.route('/community', methods=['GET','POST'])
+@app.route('/community', methods=['GET', 'POST'])
 def community():
-  return render_template('community.html')
+    return render_template('community.html')
 
-@app.route('/chats', methods=['GET','POST'])
+@app.route('/chats', methods=['GET', 'POST'])
 def chats():
-  return render_template('chats.html')
+    return render_template('chats.html')
 
-@app.route('/conncect', methods=['GET','POST'])
+@app.route('/connect', methods=['GET', 'POST'])
 def connect():
-  return render_template('connect.html')
+    return render_template('connect.html')
 '''
 
 if __name__ == '__main__':

3bb312ffa6b54eedef658e16682f8248eee68774
gabrielel24-meet <geliav2008@gmail.com>
2024-07-30 18:13:46 +0300

    feedback

--- templatemo_586_scholar/app.py
+++ templatemo_586_scholar/app.py
@@ -20,6 +20,27 @@ auth = firebase.auth()
 db = firebase.database()
 
 
+# The CODE
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        if session["user"] != None:
+            feedback = request.form['feedback']
+            uid = session['user']['localId']
+
+            ref = db.child("users").child(uid).get().val()
+
+            firstname = ref['firstname']
+            lastname = ref['lastname']
+
+            UserInfo = {'firstname': firstname, 'lastname': lastname, 'feedback': feedback}
+            db.child('feedbacks').child(uid).set(UserInfo)
+            return render_template("index.html")
+        else:
+            return render_template('login.html')
+
+    return render_template ("index.html")
   
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -72,23 +93,34 @@ def signout():
     print("signed out user")
     return redirect(url_for('index'))
 
-@app.route('/')
-def index():
-    return render_template ("index.html")
-
 
 @app.route('/jobs')
 def jobs():
     if request.method == "GET" and session["user"] != None:
         return render_template('jobs.html')
     else:
-        return render_template('signup.html')
+        return render_template('login.html')
+
+
+
+@app.route('/feedback',methods=['GET', 'POST'])
+def feedback():
+    print("im in feedback route")
+    if request.method == "POST":
+        if session["user"] != None:
+            feedback = request.form['feedback']
+            uid = user['localId']
+            UserInfo = {'firstname': firstname, 'lastname': lastname, 'feedback': feedback}
+            db.child('feedbacks').child(uid).set(UserInfo)
+            return render_template("index.html")
+        else:
+            return render_template('login.html')
+    
+
 
 @app.route('/practice')
 def practice():
     return render_template('practice.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True ,port=3000)

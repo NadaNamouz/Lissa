259ce25076926fd229f06b37e74784882318f806
nadana24-meet <nadanamouz@gmail.com>
2024-07-30 10:47:50 +0300

    update

--- templatemo_586_scholar/app.py
+++ templatemo_586_scholar/app.py
@@ -20,14 +20,21 @@ auth = firebase.auth()
 db = firebase.database()
 
 
+@app.route('/index', methods=['POST','GET'])
+def index():
+  return render_template('index.html')
 
+  
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
-        return render_template("signup.html")
-    elif request.method == "POST":
+        if not session.get("user"):
+            return render_template("signup.html")
+        else:
+            return redirect(url_for('index'))
+
+    elif request.method == 'POST':
         try:
-            print('signup')
             email = request.form['email']
             password = request.form['password']
             # firstname = request.form['firstname']
@@ -37,16 +44,21 @@ def signup():
             uid = user['localId']
             # UserInfo = {'firstname': firstname, 'lastname': lastname}
             # db.child('users').child(uid).set(UserInfo)
-            print('new page')
             return redirect(url_for('index'))
         except Exception as e:
             print(e)
+<<<<<<< Updated upstream
 
 
+=======
+            return render_template("error.html")
+    else:
+        return redirect(url_for('index'))
+>>>>>>> Stashed changes
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET' and session["user"] == None:
+    if request.method == 'GET':
         if not session.get("user"):
             return render_template("login.html")
         else:
